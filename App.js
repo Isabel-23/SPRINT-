eecb2384e6186bd92fc8647740567829d0a3e1af
Isabel-23 <112660813+Isabel-23@users.noreply.github.com>
2023-03-29 01:57:36 -0600

    Se agrega la pantalla de Login
    Se agrega al menú de navegación la pantalla de login
    
    <Stack.Screen name ="Login" component={LogIn} />

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { SafeAreaView, Text, View, StyleSheet } from 'react-native';;
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScrenn';
+import LogIn from './screens/LogIn';
 
 const Stack = createNativeStackNavigator();
 
@@ -11,6 +12,7 @@ export default function App() {
       <NavigationContainer>
        <Stack.Navigator>
          <Stack.Screen name="home" component={HomeScreen} />
+         <Stack.Screen name ="Login" component={LogIn} />
        </Stack.Navigator>
       </NavigationContainer>
   );
