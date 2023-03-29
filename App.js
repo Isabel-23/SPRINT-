566c0b7984c63055db865837ed352f205aefc6db
Isabel-23 <112660813+Isabel-23@users.noreply.github.com>
2023-03-29 01:29:46 -0600

    Update App.js

--- App.js
+++ App.js
@@ -1,22 +1,17 @@
-import { StyleSheet, Text, View } from "react-native";
-import { Provider } from "react-redux";
-import HomeScreen from "./screens/HomeScreen";
-import { store } from "./store";
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaView, Text, View, StyleSheet } from 'react-native';;
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScrenn';
 
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <Provider store={store}>
-      <HomeScreen></HomeScreen>
-    </Provider> 
+      <NavigationContainer>
+       <Stack.Navigator>
+         <Stack.Screen name="home" component={HomeScreen} />
+       </Stack.Navigator>
+      </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor:"#30E398",
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
