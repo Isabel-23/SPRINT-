0479d52487de896cf65133d95d9fd4f050d638ff
Isabel-23 <112660813+Isabel-23@users.noreply.github.com>
2023-04-24 22:38:23 -0600

    Delete assets directory

--- assets/index.js
+++
@@ -1,5 +0,0 @@
-export {default as Menu} from "./menu.png"
-export {default as avatar} from "./usuario.png"
-export {default as Mapa} from "./mapa.png"
-export {default as Ruta} from "./furgoneta.png"
-export {default as icono} from "./rutaicon.png"
